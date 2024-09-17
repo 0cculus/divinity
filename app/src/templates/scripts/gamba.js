17fb8374a7bf74a482ac79d443101caf162c0963
bri.rhm@gmail.com <brheaume@c1r6p5.42quebec.com>
2024-09-17 18:36:03 -0400

    started second game & updated logs

---
+++ app/src/templates/scripts/gamba.js
@@ -0,0 +1,18 @@
+var nbDiceAvailable;
+var nbDiceSaved;
+
+function init()
+{
+	var contentElement = document.getElementById("content");
+	nbDiceAvailable = 6;
+	nbDiceSaved = 0;
+
+	contentElement.innerHTML = ""
+}
+
+function rerollDice()
+{
+	if (nbDiceAvailable == 0 && nbDiceSaved == 6)
+	{
+	}
+}
