2ca6ecd3bf005d37d8833f5b6785db0321262199
bri.rhm@gmail.com <brheaume@c1r7p2.42quebec.com>
2024-12-06 18:26:47 -0500

    merged threejs with logic

--- app/src/static/js/gamba.js
+++ app/src/static/js/gamba.js
@@ -1,19 +1,82 @@
 import { Player } from './userGamba.js'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { Box } from '../game/box.js'
 
 var tabDiceAvail = Array(6)
 var tabDiceSaved = Array(6)
 var tabPlayer = Array(2)
 var tabLimbo = Array(6)
 var tabScore = Array()
+var dices = Array(6)
 
 var currentSaved = 0
 var currentLimbo = 0
-//var currentScore = 0
 var playerCount = 0
 var currentPlayer = null
+var currentCursor = 0
+let frames = 0
+let spawnRate = 200
 const minVal = 1
 const maxVal = 6
 const nbDice = 6
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(
+	72,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	10000
+)
+camera.position.set(0, 0, 9)
+
+const renderer = new THREE.WebGLRenderer({
+	alpha: true,
+	antialias: true
+})
+renderer.shadowMap.enabled = true
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.getElementById("content").appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enabled = false
+
+const cursor = new Box({
+	width: 0.3,
+	height: 0.3,
+	depth: 0.3,
+	color: '#000',
+	position: {
+		x: -5,
+		y: 0.5,
+		z: 0
+	}
+})
+cursor.castShadow = true
+scene.add(cursor)
+
+const ground = new Box({
+	width: 100,
+	height: 0.1,
+	depth: 50,
+	color: '#866600',
+	position: {
+		x: 0,
+		y: -2,
+		z: 0
+	}
+})
+ground.receiveShadow = true
+scene.add(ground)
+
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.y = 3
+light.position.z = 1
+light.castShadow = true
+scene.add(light)
+scene.add(new THREE.AmbientLight(0xffffff, 0.5))
+
+camera.position.z = 5
 
 /* ----------------------------------- */
 // WIP ADD END CONDITION & TIE BREAKER //
@@ -21,15 +84,30 @@ const nbDice = 6
 
 function save(id)
 {
-	//let index = Number(id.replace("availDie", ""))
-	if (currentLimbo < 6 && tabDiceAvail[index] != 0)
+	if (currentLimbo < 6 && tabDiceAvail[currentCursor] != 0)
 	{
-		tabLimbo[currentLimbo] = tabDiceAvail[index]
-		tabDiceAvail[index] = 0
-		//document.getElementById("limboDie" + currentLimbo).value = tabLimbo[currentLimbo]
-		//document.getElementById(id).value = ""
+		tabLimbo[currentLimbo] = tabDiceAvail[currentCursor]
+		dices[currentCursor].position.set(dices[currentCursor].position.x, dices[currentCursor].position.y, dices[currentCursor].position.z - 2) 
+		tabDiceAvail[currentCursor] = 0
 		currentLimbo++
 	}
+}
+
+function clearDices()
+{
+	for (var i = 0; i < dices.length; i++)
+	{
+		dices[i].kill()
+		scene.remove(dices[i])
+	}
+	for (var i = 0; i < nbDice; i++)
+	{
+		tabDiceAvail[i] = 0
+		tabDiceSaved[i] = 0
+		tabLimbo[i] = 0
+	}
+	currentSaved = 0
+	currentLimbo = 0
 }
 
 function cacheSaved()
@@ -40,8 +118,6 @@ function cacheSaved()
 		tabDiceSaved[currentSaved] = tabLimbo[i]
 		tabLimbo[i] = 0
 		currentScore.push(tabDiceSaved[currentSaved])
-		//document.getElementById("savedDie" + currentSaved).value = tabDiceSaved[currentSaved]
-		//document.getElementById("limboDie" + i).value = ""
 		currentSaved++
 	}
 	tabScore.push(currentScore)
@@ -53,40 +129,28 @@ function rerollDice()
 {
 	for (var i = 0; i < tabDiceAvail.length; i++)
 	{
-		//document.getElementById("availDie" + i).value = ""
 		tabDiceAvail[i] = 0
 	}
-	if (currentSaved < nbDice)
+	for (var i = 0; i < nbDice - currentSaved; i++)
 	{
-		for (var i = 0; i < nbDice - currentSaved; i++)
-		{
-			tabDiceAvail[i] = Math.floor(Math.random() * (maxVal -minVal) + minVal)
-			//document.getElementById("availDie" + i).value = tabDiceAvail[i]
-		}
+		tabDiceAvail[i] = Math.floor(Math.random() * (maxVal -minVal) + minVal)
+		dices[i] = new Box({
+			width: 1,
+			height: 1,
+			depth: 1,
+			color: "#7a02e3",
+			position: {
+				x: -5 + (2 * i),
+				e: 20,
+				z: 0
+			}
+		})
+		dices[i].gravity = -0.022
+		dices[i].castShadow = true
+		dices[i].receiveShadow = true
+		scene.add(dices[i])
 	}
-	else
-	{
-		for (var i = 0; i < tabDiceAvail.length; i++)
-		{
-			tabDiceAvail[i] = Math.floor(Math.random() * (maxVal -minVal) + minVal)
-			//document.getElementById("availDie" + i).value = tabDiceAvail[i]
-		}
-	}
-}
-
-function clear()
-{
-	for (var i = 0; i < nbDice; i++)
-	{
-		//document.getElementById("availDie" + i).value = ""
-		//document.getElementById("savedDie" + i).value = ""
-		//document.getElementById("limboDie" + i).value = ""
-		tabDiceAvail[i] = 0
-		tabDiceSaved[i] = 0
-		tabLimbo[i] = 0
-	}
-	currentSaved = 0
-	currentLimbo = 0
+	console.log(tabDiceAvail)
 }
 
 function countStreak(base, amount)
@@ -145,7 +209,7 @@ function count(arr)
 function score()
 {
 	if (currentLimbo == 0)
-		clear()
+		clearDices()
 	else if (currentSaved != 0 || currentLimbo != 0)
 	{
 		tabLimbo.sort()
@@ -157,73 +221,102 @@ function score()
 	tabScore = []
 	playerCount++
 	currentPlayer = tabPlayer[playerCount % 2]
-	clear()
+	clearDices()
 	rerollDice()
 }
+
+function moveCursor(direction)
+{
+	if (currentCursor < 0)
+	{
+		currentCursor = 5
+		cursor.position.set(5, cursor.position.y, cursor.position.z)
+	}
+	else if (currentCursor > 5)
+	{
+		currentCursor = 0
+		cursor.position.set(-5, cursor.position.y, cursor.position.z)
+	}
+	else
+		cursor.position.set(cursor.position.x + direction, cursor.position.y, cursor.position.z)
+}
+
+window.addEventListener('keydown', (event) => {
+	switch (event.code) {
+		case 'ArrowLeft':
+		case 'KeyA':
+			currentCursor--;
+			moveCursor(-2)
+			console.log("move cursor left")
+			break
+		case 'ArrowRight':
+		case 'KeyD':
+			currentCursor++;
+			moveCursor(2)
+			console.log("move cursor right")
+			break
+		case 'KeyR':
+			console.log("reroll")
+			rerollDice()
+			break
+		case 'Space':
+			console.log("select die: ", currentCursor)
+			console.log("selected value: ", tabDiceAvail[currentCursor])
+			save()
+			break
+		case 'Enter':
+			score()
+			console.log("end turn")
+			break
+	}
+})
 
 function init()
 {
 	const contentElement = document.getElementById("content")
-
-	/*contentElement.innerHTML = `<div id="avail">
-</div>
-<div id="limbo">
-</div>
-<div id="saved">
-</div>
-<div id="controls">
-</div>`
-
-	const divAvail = document.getElementById("avail")
-	for (var i = 0; i < tabDiceAvail.length; i++)
+	
+	console.log("radar")
+	for (var i = 0; i < nbDice; i++)
 	{
-		const die = document.createElement("input")
 		tabDiceAvail[i] = Math.floor(Math.random() * (maxVal -minVal) + minVal)
-		die.value = tabDiceAvail[i]
-		die.type = "button"
-		die.id = "availDie" + i
-		die.addEventListener("click", function(){
-			save(die.id)
+		dices[i] = new Box({
+			width: 1,
+			height: 1,
+			depth: 1,
+			color: "#7a02e3",
+			position: {
+				x: -5 + (2 * i),
+				e: 20,
+				z: 0
+			}
 		})
-		divAvail.appendChild(die)
+		dices[i].gravity = -0.022
+		dices[i].castShadow = true
+		dices[i].receiveShadow = true
+		scene.add(dices[i])
 	}
-	const divLimbo = document.getElementById("limbo")
-	for (var i = 0; i < tabDiceSaved.length; i++)
-	{
-		const die = document.createElement("input")
-		die.type = "button"
-		die.id = "limboDie" + i
-		divLimbo.appendChild(die)
-	}
-	const divSaved = document.getElementById("saved")
-	for (var i = 0; i < tabDiceSaved.length; i++)
-	{
-		const die = document.createElement("input")
-		die.type = "button"
-		die.id = "savedDie" + i
-		divSaved.appendChild(die)
-	}
-	const endTurn = document.createElement("input")
-	endTurn.type = "button"
-	endTurn.id = "btnEnd"
-	endTurn.value = "end turn"
-	endTurn.addEventListener("click", function() {
-		score()
-	})
-	document.getElementById("controls").appendChild(endTurn)
-	const saveDice = document.createElement("input")
-	saveDice.type = "button"
-	saveDice.id = "btnSave"
-	saveDice.value = "save dice"
-	saveDice.addEventListener("click", function() {
-		cacheSaved()
-	})
-	document.getElementById("controls").appendChild(saveDice)*/
-
+	console.log(tabDiceAvail)
 	playerCount = 0
 	tabPlayer[0] = new Player("Cathan")
 	tabPlayer[1] = new Player("Felix")
 	currentPlayer = tabPlayer[Math.floor(Math.random() * 2)]
 }
 
+function animate() {
+	const animationId = requestAnimationFrame(animate)
+	renderer.render(scene, camera)
+
+	// movement code
+	for (let i = 0; i < 6; i++)
+	{
+		dices[i].velocity.x = 0
+		dices[i].velocity.z = 0
+
+		dices[i].update(ground)
+	}
+
+	frames++
+}
+
 init()
+animate()
