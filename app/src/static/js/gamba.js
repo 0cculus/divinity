556b102f4124007e434be0ae36b1bef620339fa7
bri.rhm@gmail.com <brheaume@c1r7p2.42quebec.com>
2024-12-12 20:07:15 -0500

    fixed logic problems & improved game logic, added end conditions

--- app/src/static/js/gamba.js
+++ app/src/static/js/gamba.js
@@ -15,11 +15,25 @@ var currentLimbo = 0
 var playerCount = 0
 var currentPlayer = null
 var currentCursor = 0
-let frames = 0
-let spawnRate = 200
+var frames = 0
+var spawnRate = 200
+var gameEnd = false
+var finalRoll = false
 const minVal = 1
 const maxVal = 6
 const nbDice = 6
+
+/* ------------------------------------------ */
+// WIP ADD TEXTURES + TEXT FOR NAMES & SCORES //
+/* ------------------------------------------ */
+
+/* ------------------------------------------------- */
+// WIP ADD CONDITION CHECK FOR SINGLE DIGIT IN LIMBO //
+/* ------------------------------------------------- */
+
+/* ---------------------------------------------- */
+// WIP REMOVE EVENT LISTENERS AFTER WIN CONDITION //
+/* ---------------------------------------------- */
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
@@ -38,8 +52,8 @@ renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.getElementById("content").appendChild(renderer.domElement)
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enabled = true
+const sceneControls = new OrbitControls(camera, renderer.domElement)
+sceneControls.enabled = false
 
 const cursor = new Box({
 	width: 0.3,
@@ -78,11 +92,7 @@ scene.add(new THREE.AmbientLight(0xffffff, 0.5))
 
 camera.position.z = 5
 
-/* ----------------------------------- */
-// WIP ADD END CONDITION & TIE BREAKER //
-/* ----------------------------------- */
-
-function reserveDice(id)
+function reserveDice()
 {
 	if (currentLimbo < 6 && tabDiceAvail[currentCursor] != 0)
 	{
@@ -91,6 +101,29 @@ function reserveDice(id)
 		tabDiceAvail[currentCursor] = 0
 		currentLimbo++
 	}
+}
+
+function verifyPlayableRoll()
+{
+	var tabCheck = [2, 3, 4, 6]
+	var tabCount = [0, 0, 0, 0]
+	var lessThanThree = 0
+
+	for (var i = 0; i < tabCheck.length; i++)
+	{
+		for (var j = 0; j < tabDiceAvail.length; j++)
+		{
+			if (tabCheck[i] == tabDiceAvail[j])
+				tabCount[i]++
+		}
+	}
+	for (var i = 0; i < tabCount.length; i++)
+	{
+		if (tabCount[i] < 3)
+			lessThanThree++
+	}
+	if (lessThanThree == 3)
+		tabDiceAvail[Math.floor(Math.random() * (maxVal - minVal))] = 5
 }
 
 function clearDices()
@@ -111,16 +144,27 @@ function clearDices()
 function cacheSaved()
 {
 	var currentScore = Array()
-	for (var i = 0; i < currentLimbo && currentSaved < nbDice; i++)
+	if (currentLimbo == 0)
+		score()
+	else
 	{
-		tabDiceSaved[currentSaved] = tabLimbo[i]
-		tabLimbo[i] = 0
-		currentScore.push(tabDiceSaved[currentSaved])
-		currentSaved++
+		for (var i = 0; i < currentLimbo && currentSaved < nbDice; i++)
+		{
+			tabDiceSaved[currentSaved] = tabLimbo[i]
+			tabLimbo[i] = 0
+			currentScore.push(tabDiceSaved[currentSaved])
+			currentSaved++
+		}
+		tabScore.push(currentScore)
+		currentLimbo = 0
+		if (finalRoll)
+		{
+			score()
+			finalRoll = false
+		}
+		else
+			rerollDice()
 	}
-	tabScore.push(currentScore)
-	currentLimbo = 0
-	rerollDice()
 }
 
 function rerollDice()
@@ -130,9 +174,14 @@ function rerollDice()
 		tabDiceAvail[i] = 0
 	}
 	clearDices()
+	if (currentSaved == 6)
+	{
+		currentSaved = 0
+		finalRoll = true
+	}
 	for (var i = 0; i < nbDice - currentSaved; i++)
 	{
-		tabDiceAvail[i] = Math.floor(Math.random() * (maxVal -minVal) + minVal)
+		tabDiceAvail[i] = Math.floor(Math.random() * (maxVal - minVal) + minVal)
 		dices[i] = new Box({
 			width: 1,
 			height: 1,
@@ -149,6 +198,7 @@ function rerollDice()
 		dices[i].receiveShadow = true
 		scene.add(dices[i])
 	}
+	verifyPlayableRoll()
 	console.log(tabDiceAvail)
 }
 
@@ -215,6 +265,7 @@ function count(arr)
 
 function score()
 {
+	finalRoll = false
 	if (currentLimbo == 0)
 		clearAll()
 	else if (currentSaved != 0 || currentLimbo != 0)
@@ -223,13 +274,15 @@ function score()
 		for (var i = 0; i < tabScore.length; i++)
 			currentPlayer.setScore(currentPlayer.getScore() + Number(count(tabScore[i])))
 		currentPlayer.setScore(currentPlayer.getScore() + Number(count(tabLimbo)))
-		console.log(currentPlayer)
 	}
+	checkEnd()
 	tabScore = []
 	playerCount++
 	currentPlayer = tabPlayer[playerCount % 2]
+	console.log(currentPlayer)
 	clearAll()
-	rerollDice()
+	if (!gameEnd)
+		rerollDice()
 }
 
 function moveCursor(direction)
@@ -248,32 +301,51 @@ function moveCursor(direction)
 		cursor.position.set(cursor.position.x + direction, cursor.position.y, cursor.position.z)
 }
 
-window.addEventListener('keydown', (event) => {
-	switch (event.code) {
-		case 'ArrowLeft':
-		case 'KeyA':
-			currentCursor--;
-			moveCursor(-2)
-			break
-		case 'ArrowRight':
-		case 'KeyD':
-			currentCursor++;
-			moveCursor(2)
-			break
-		case 'KeyR':
-			cacheSaved()
-			break
-		case 'Space':
-			console.log("select die: ", currentCursor)
-			console.log("selected value: ", tabDiceAvail[currentCursor])
-			reserveDice()
-			break
-		case 'Enter':
-			console.log("end turn")
-			score()
-			break
+window.addEventListener('keydown', gameControls)
+
+function gameControls(event)
+{
+	if (gameEnd == false)
+	{
+		switch (event.code) {
+			case 'ArrowLeft':
+			case 'KeyA':
+				currentCursor--;
+				moveCursor(-2)
+				break
+			case 'ArrowRight':
+			case 'KeyD':
+				currentCursor++;
+				moveCursor(2)
+				break
+			case 'KeyR':
+				cacheSaved()
+				break
+			case 'Space':
+				console.log("selected value: ", tabDiceAvail[currentCursor])
+				reserveDice()
+				break
+			case 'Enter':
+				console.log("end turn")
+				score()
+				break
+		}
 	}
-})
+}
+
+function checkEnd()
+{
+	if (tabPlayer[0].score >= 3000)
+	{
+		console.log(tabPlayer[0].name)
+		gameEnd = true
+	}
+	else if (tabPlayer[1].score >= 3000)
+	{
+		console.log(tabPlayer[1].name)
+		gameEnd = true
+	}
+}
 
 function init()
 {
@@ -281,7 +353,7 @@ function init()
 	
 	for (var i = 0; i < nbDice; i++)
 	{
-		tabDiceAvail[i] = Math.floor(Math.random() * (maxVal -minVal) + minVal)
+		tabDiceAvail[i] = Math.floor(Math.random() * (maxVal) + minVal)
 		dices[i] = new Box({
 			width: 1,
 			height: 1,
@@ -299,10 +371,11 @@ function init()
 		scene.add(dices[i])
 	}
 	console.log(tabDiceAvail)
-	playerCount = 0
+	playerCount = Math.floor(Math.random() * 2)
 	tabPlayer[0] = new Player("Cathan")
 	tabPlayer[1] = new Player("Felix")
-	currentPlayer = tabPlayer[Math.floor(Math.random() * 2)]
+	currentPlayer = tabPlayer[playerCount % 2]
+	console.log(currentPlayer)
 }
 
 function animate() {
